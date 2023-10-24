df56e0954875e333e5720b8f56f79ea7b99f5252
wagner200172 <wagner200172@sr.htlweiz.at>
2023-10-24 12:00:00 +0200

    server client exchange

---
+++ code/server.py
@@ -0,0 +1,30 @@
+import socket
+ 
+ 
+def server_program():
+    host = socket.gethostname()
+    port = 9979  
+
+    server_socket = socket.socket()
+   
+    server_socket.bind((host, port))  
+
+    server_socket.listen(2)
+    conn, address = server_socket.accept()
+    print("connected")
+    print("Connection from: " + str(address))
+    while True:
+     
+        data = conn.recv(1024).decode()
+        if not data:
+          
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  
+ 
+    conn.close()  
+ 
+ 
+if __name__ == '__main__':
+    server_program()
